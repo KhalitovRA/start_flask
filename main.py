a1d94bbc2af879299d989f6ae8b4ac07cd0ecac2
KhalitovRA <halitov-02@mail.ru>
2022-04-10 20:53:05 +0300

    start creating user login system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, url_for, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'qwertyuiop'
@@ -20,6 +20,8 @@ def about():
 
 @app.route('/profile/<username>')
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return f'user {username}'
 
 
@@ -34,6 +36,22 @@ def support():
             flash('Ошибка отправки', category='error')
 
     return render_template('support.html', title='Поддержка', menu=menu)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'selfedu@mail.ru' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
 
 
 if __name__ == '__main__':
