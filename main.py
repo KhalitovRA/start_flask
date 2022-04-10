96446cb2a49b8b156b74982c62f9c4d893824fd0
KhalitovRA <halitov-02@mail.ru>
2022-04-10 20:07:57 +0300

    work with html, css and forms

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
-menu = ['О нас', 'support', 'contacts']
+app.config['SECRET_KEY'] = 'qwertyuiop'
+menu = [{'name': 'Главная страница', 'url': '/'},
+        {'name': 'О нас', 'url': '/about'},
+        {'name': 'Поддержка', 'url': '/support'},]
 
 
 @app.route('/index')
 @app.route('/')
 def index():
-    print(url_for('index'))
     return render_template('index.html', title='Главная страница', menu=menu)
 
 
 @app.route('/about')
 def about():
-    print(url_for('about'))
-    return render_template('about.html', title='О нашем сайте')
+    return render_template('about.html', title='О нашем сайте', menu=menu)
 
 
 @app.route('/profile/<username>')
 def profile(username):
     return f'user {username}'
+
+
+@app.route('/support', methods=['POST', 'GET'])
+def support():
+
+    if request.method == 'POST':
+        print(request.form)
+        if len(request.form['message']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('support.html', title='Поддержка', menu=menu)
 
 
 if __name__ == '__main__':
